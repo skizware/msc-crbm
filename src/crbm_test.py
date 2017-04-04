a8fd8ec41ff8644960ff54a6d03040f03f73f45b
Dave Anderson <djohnanderson@gmail.com>
2017-04-04 23:20:24 +0200

    update test script for batching, which doesnt work lol

--- src/crbm_test.py
+++ src/crbm_test.py
@@ -5,6 +5,9 @@ import crbm
 from layer import BinaryVisibleNonPooledLayer
 from datetime import datetime
 
+BATCH_SIZE = 1
+
+myLayer = None
 
 def load_data():
     f = gzip.open("/home/dave/Downloads/mnist.pkl.gz", "rb")
@@ -13,17 +16,18 @@ def load_data():
     return [train_set, valid_set, test_set]
 
 
-def testRbm():
+def testRbm(theLayer):
     train_set, _, _ = load_data()
-    myLayer = BinaryVisibleNonPooledLayer((1, 1, 28, 28), (1, 40, 19, 19), target_sparsity=0.1,
-                                          sparsity_learning_rate=0.1)
-    for index in range(0, train_set[0].shape[0]):
-        if (index % 1000 == 0):
-            print("Training on sample {} at time {}".format(index, datetime.now()))
-        imgMat = np.reshape(train_set[0][index], (1, 1, 28, 28))
-        myLayer.train_on_minibatch(imgMat)
+    train_set = train_set[0].reshape(50000, 1, 28, 28)
+
+    for index in range(0, train_set.shape[0]/ BATCH_SIZE):
+        if(index % 1000 == 0):
+            print("Training on sample {} at time {}".format(index * BATCH_SIZE, datetime.now()))
+        batch = train_set[index * BATCH_SIZE:(index + 1) * BATCH_SIZE, :, :, :]
+        theLayer.train_on_minibatch(batch)
 
     print "Training ended at {}".format(datetime.now())
 
-
-testRbm()
+myLayer = BinaryVisibleNonPooledLayer((1, 1, 28, 28), (1, 40, 19, 19), target_sparsity=0.1,
+                                          sparsity_learning_rate=0.1)
+testRbm(myLayer)
